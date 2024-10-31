487344e703c72b2479c99d9dec837e548d552c00
Joshua Han <joshhan619@gmail.com>
2024-10-30 21:59:16 -0500

    chg: added RGRRT to visualization code

--- src/visualize.py
+++ src/visualize.py
@@ -44,15 +44,15 @@ def visualize_pendulum(data1, data2, data3, planner):
     plt.suptitle(f"{planner} pendulum solution trajectory")
     plt.show()
 
-def visualize_car(data1, data2):
-    fig, ax = plt.subplots(1,2, sharey=True)
+def visualize_car(data1, data2, data3):
+    fig, ax = plt.subplots(1,3, sharey=True)
     # Rectangular obstacles are tuples (x, y, w, h) where (x,y) is coordinate of lower left corner
     obstacles = [(-6, -10, 10, 4), (-6, 6, 10, 2), (-6, -4, 5, 8), (2, -4, 4, 8)]
 
     # Visualize the same start and goal states for all graphs
     start = np.array([[-5], [-5]])
     goal = np.array([[4], [5]])
-    for i in range(2):
+    for i in range(3):
         ax[i].set_xbound(-6, 6)
         ax[i].set_ybound(-10, 10)
         for obs in obstacles:
@@ -86,6 +86,17 @@ def visualize_car(data1, data2):
         # Draw an arrow in the direction of the robot's orientation
         arrow = np.array([[math.cos(theta)], [math.sin(theta)]])
         ax[1].arrow(curr_pos[0, 0], curr_pos[1, 0], arrow[0, 0], arrow[1, 0], head_width=0.1)
+
+    # Plot RGRRT trajectory
+    ax[2].plot(data3[0], data3[1], 'b-')
+    ax[2].title.set_text(f"RGRRT car solution trajectory")
+    for i in range(data3.shape[0]):
+        theta = data3.iloc[i, 2]
+        curr_pos = data3.iloc[i, :2].to_numpy().reshape((2, 1))
+        # Draw an arrow in the direction of the robot's orientation
+        arrow = np.array([[math.cos(theta)], [math.sin(theta)]])
+        ax[2].arrow(curr_pos[0, 0], curr_pos[1, 0], arrow[0, 0], arrow[1, 0], head_width=0.1)
+
     
     fig.text(0.5, 0.04, "x", ha="center")
     fig.text(0.04, 0.5, "y", va="center", rotation="vertical")
@@ -106,7 +117,15 @@ if __name__ == '__main__':
     data6 = read_geometric_path(f"{planner}_pendulum_t10.txt")
     visualize_pendulum(data4, data5, data6, planner)
 
+    # Visualize KPIECE planner solutions for each torque in the pendulum system
+    planner = 'RGRRT'
+    data7 = read_geometric_path(f"{planner}_pendulum_t3.txt")
+    data8 = read_geometric_path(f"{planner}_pendulum_t5.txt")
+    data9 = read_geometric_path(f"{planner}_pendulum_t10.txt")
+    visualize_pendulum(data7, data8, data9, planner)
+
     # Visualize RRT and KPIECE solutions in the car system
-    data7 = read_geometric_path("RRT_car.txt")
-    data8 = read_geometric_path("KPIECE_car.txt")
-    visualize_car(data7, data8)
+    data10 = read_geometric_path("RRT_car.txt")
+    data11 = read_geometric_path("KPIECE_car.txt")
+    data12 = read_geometric_path("RGRRT_car.txt")
+    visualize_car(data10, data11, data12)
